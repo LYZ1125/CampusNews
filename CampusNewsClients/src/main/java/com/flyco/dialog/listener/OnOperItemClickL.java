d4f2b2c2269d4a19086eeb7e660933f539638a3c
Thomas <1165467740@qq.com>
2017-04-06 09:31:02 +0800

    delete a lib

--- CampusNewsClients/src/main/java/com/flyco/dialog/listener/OnOperItemClickL.java
+++
@@ -1,8 +0,0 @@
-package com.flyco.dialog.listener;
-
-import android.view.View;
-import android.widget.AdapterView;
-
-public interface OnOperItemClickL {
-		void onOperItemClick(AdapterView<?> parent, View view, int position, long id);
-	}
